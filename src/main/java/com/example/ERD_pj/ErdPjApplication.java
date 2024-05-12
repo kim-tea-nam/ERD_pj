fdd6344a63bd468de213f2efd8d50fe5df85d40b
taenam kim <ktn2674@naver.com>
2024-05-12 22:16:12 +0900

    erd 처음

---
+++ src/main/java/com/example/ERD_pj/ErdPjApplication.java
@@ -0,0 +1,13 @@
+package com.example.ERD_pj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErdPjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErdPjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ERD_pj/ErdPjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ERD_pj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErdPjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
